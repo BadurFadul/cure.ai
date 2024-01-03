772af0bfe1d64afbd217233f67e58417b9d96917
bad fad <badfad@edu.novia.fi>
2024-01-03 17:55:18 +0200

    Add llm template

---
+++ llm-api/utils.py
@@ -0,0 +1,10 @@
+import json
+
+
+def get_request(event):
+    request = event.get("body")
+    if request:
+        request = json.loads(request)
+    else:
+        request = event
+    return request
